2811e0f8d3de14c742b1c5888097294141247f70
VyacheslavRadyuk <vyacheslavradyuk@mail.ru>
2021-06-07 16:13:40 +0300

    added first task from lecture_3.4

---
+++ lecture_3.4/RegistrationOfTripsController.js
@@ -0,0 +1,33 @@
+({
+    doInit : function(component, event, helper) {
+        component.set("v.columns", [
+            {label: 'Tourist Name', fieldName: 'Name', type: 'text'},
+            {label: 'Email', fieldName: 'Email__c', type: 'email'},
+            {label: 'Gender', fieldName: 'Gender__c', type: 'picklist'}
+        ]);
+        helper.fetchTourist(component, event);     
+    },
+    
+    handleClick : function (component, event, helper) {
+        const selectedEventId = event.target.id;
+        const msg ='Are you sure you want to create flight for this tourists?';
+        
+        if (!confirm(msg)) {
+            return false;
+        } else {   
+            helper.createFlights(component, event);          
+        }   
+    },
+    
+    doSelectRecord : function(component, event, helper) {
+        helper.selectRecords(component, event);
+    },
+    
+    showSpinner: function(component, event, helper) {
+        component.set("v.spinner", true); 
+    },
+    
+    hideSpinner : function(component,event,helper){  
+        component.set("v.spinner", false);
+    }
+})
---
+++ lecture_3.4/RegistrationOfTripsHelper.js
@@ -0,0 +1,58 @@
+({
+    fetchTourist : function(component, event) {    
+        var action = component.get('c.fetchTourist');
+        action.setParams({
+            tripRecordId: component.get('v.recordId')
+        });
+        action.setCallback(this, function(response) {
+            var state = response.getState();
+            if (state === 'SUCCESS') {
+                var responseValue = response.getReturnValue();
+                component.set('v.data', responseValue);
+            }
+        });
+        $A.enqueueAction(action);       
+    },
+    
+    createFlights : function(component, event) {
+        var action = component.get('c.createFlights');
+        action.setParams({
+            tripRecordId: component.get('v.recordId'),
+            selectedTourists: component.get('v.selectedTourists')
+        });
+        action.setCallback(this, function(response) {
+            var state = response.getState();
+            if (state === "SUCCESS") {
+                var responseValue = response.getReturnValue();
+                if (responseValue.length > 0) {
+                    var toastSuccess = $A.get("e.force:showToast");
+                    toastSuccess.setParams({
+                        message: $A.get("$Label.c.createFlights"),
+                        duration: 3000,
+                        type: 'success'
+                    });
+                    toastSuccess.fire();
+                } else {
+                    var toastError = $A.get("e.force:showToast");
+                    toastError.setParams({
+                        message: $A.get("$Label.c.createFlightsError"),
+                        duration: 3000,
+                        type: 'error'
+                    });
+                    toastError.fire();
+                }
+            }  
+        });
+        $A.enqueueAction(action);
+    },
+    
+    selectRecords : function(component, event) {
+        var selectedRows = event.getParam('selectedRows'); 
+        var setRows = [];
+        
+        selectedRows.forEach(function(selectedRow) {
+            setRows.push(selectedRow);
+        })        
+        component.set("v.selectedTourists", setRows);
+    }
+})
