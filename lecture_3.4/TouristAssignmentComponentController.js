8ae3961d2c138f66875a8f8382702753d15674f2
VyacheslavRadyuk <vyacheslavradyuk@mail.ru>
2021-06-11 20:58:02 +0300

    added 2 task (Tourist Assignment app)

---
+++ lecture_3.4/TouristAssignmentComponentController.js
@@ -0,0 +1,58 @@
+({
+    doInit : function(component, event, helper) {
+        component.set("v.columnsTourist", [
+            {label: 'Tourist Name', fieldName: 'linkName', type: 'url', 
+             typeAttributes: {label: {fieldName: 'Name'}, target: '_blank'}},
+            {label: 'Email', fieldName: 'Email__c', type: 'email'},
+            {label: 'Gender', fieldName: 'Gender__c', type: 'picklist'}
+        ]); 
+        component.set('v.isNoActiveButton', true);
+        helper.fetchTourist(component, event);
+        component.set("v.isActiveGeolocationOfSpacePoint", false);    
+    },  
+    
+    doSelectRecordTourist : function(component, event, helper) {
+        helper.selectRecordsTourist(component, event);
+        helper.fetchTrip(component, event);
+        component.set("v.columnsTrip", [
+            {label: 'Trip Name', fieldName: 'linkName', type: 'url', 
+             typeAttributes: {label: {fieldName: 'Name'}, target: '_blank'}},
+            {label: 'Start date', fieldName: 'Start_Date__c', type: 'date'}
+        ]); 
+        component.set("v.isActiveGeolocationOfSpacePoint", false);
+        component.set("v.isNoActiveButton", true);
+    },
+    
+    doSelectRecordTrip : function(component, event, helper) {
+        helper.selectRecordsTrip(component, event);
+        helper.fetchFlights(component, event);
+    },
+    
+    handleClick : function(component, event, helper) {
+        const action = component.get('c.createFlightForTouristAssignment');
+        const selectedTrip = component.get('v.selectedTrip');
+        const selectedTourist = component.get('v.selectedTourist');
+        action.setParams({
+            selectedTrip: selectedTrip[0],
+            selectedTourist: selectedTourist[0]
+        });
+        action.setCallback(this, function(response) {
+            const state = response.getState();
+            if (state === "SUCCESS") {
+                const responseValue = response.getReturnValue();
+                if (responseValue.length != 0) {
+                    const toastSuccess = $A.get("e.force:showToast");
+                    toastSuccess.setParams({
+                        message: $A.get("$Label.c.createFlights"),
+                        duration: 3000,
+                        type: $A.get("$Label.c.toastTypeSuccessfully")
+                    });
+                    toastSuccess.fire();
+                } 
+            }  
+        });
+        $A.enqueueAction(action);
+        component.set('v.refreshApp', true);
+        component.set('v.refreshApp', false);        
+    }
+})
---
+++ lecture_3.4/TouristAssignmentComponentHelper.js
@@ -0,0 +1,100 @@
+({
+    fetchTourist : function(component, event) {    
+        const action = component.get('c.getTourists');
+        action.setCallback(this, function(response) {
+            const state = response.getState();
+            if (state === 'SUCCESS') {
+                const responseValue = response.getReturnValue();
+                responseValue.forEach(function(value){
+                    value.linkName = '/' + value.Id;
+                })
+                component.set('v.dataTourist', responseValue);
+            }
+        });
+        $A.enqueueAction(action); 
+    },
+    
+    fetchTrip : function(component, event) {    
+        const action = component.get('c.getTrips');
+        const selectedTourist = component.get('v.selectedTourist');
+        action.setParams({            
+            selectedTourist: selectedTourist[0]
+        });
+        action.setCallback(this, function(response) {
+            const state = response.getState();
+            if (state === 'SUCCESS') {
+                const responseValue = response.getReturnValue();
+                responseValue.forEach(function(value){
+                    value.linkName = '/' + value.Id;
+                })
+                component.set('v.dataTrip', responseValue);                            
+            }               
+        });
+        $A.enqueueAction(action); 
+    },
+    
+    selectRecordsTourist : function(component, event) {
+        const selectedRows = event.getParam('selectedRows'); 
+        let setRows = [];
+        
+        selectedRows.forEach(function(selectedRow) {
+            setRows.push(selectedRow);
+        })        
+        component.set("v.selectedTourist", setRows); 
+        component.set("v.selectedTrip", []);
+        component.set("v.refreshTripDatatable", true);
+        component.set("v.refreshTripDatatable", false);
+    },
+    
+    selectRecordsTrip : function(component, event) {
+        const selectedRows = event.getParam('selectedRows'); 
+        let setRows = [];
+        
+        selectedRows.forEach(function(selectedRow) {
+            setRows.push(selectedRow);
+        })        
+        component.set("v.selectedTrip", setRows);
+        component.set("v.isNoActiveButton", false);
+        const trip =  component.get('v.selectedTrip');       
+        component.set('v.mapMarkers', [            
+            {
+                location: {
+                    Latitude: trip[0].Departure_Space_Point__r.Latitude__c,
+                    Longitude: trip[0].Departure_Space_Point__r.Longitude__c
+                }
+            }
+        ]);
+        component.set("v.isActiveGeolocationOfSpacePoint", true);
+        
+        const action = component.get('c.getWeatherForecastBySpacePoint');
+        const selectedTrip = component.get('v.selectedTrip');
+        action.setParams({            
+            selectedTrip: selectedTrip[0]
+        });
+        action.setCallback(this, function(response) {
+            const state = response.getState();
+            if (state === 'SUCCESS') {
+                const responseValue = response.getReturnValue();
+                component.set("v.date", responseValue.Date__c); 
+                component.set("v.averageTemperature", responseValue.Average_Temperature__c);           
+            }               
+        });   
+        $A.enqueueAction(action); 
+    },
+    
+    fetchFlights : function(component, event) {
+        const action = component.get('c.getFlightsBySelectedTrip');
+        const selectedTrip = component.get('v.selectedTrip');
+        action.setParams({            
+            selectedTrip: selectedTrip[0]
+        });
+        action.setCallback(this, function(response) {
+            const state = response.getState();
+            if (state === 'SUCCESS') {
+                const responseValue = response.getReturnValue();
+                component.set("v.countOccupiedSeats", responseValue.length);          
+            }               
+        });   
+        $A.enqueueAction(action); 
+    }
+})
