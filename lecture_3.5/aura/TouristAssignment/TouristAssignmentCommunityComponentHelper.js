8d83e4c91e7f4bbfbe865a0493cad0c4ec4a18cd
VyacheslavRadyuk <vyacheslavradyuk@mail.ru>
2021-06-30 21:35:15 +0300

    var->let

--- lecture_3.5/aura/TouristAssignment/TouristAssignmentCommunityComponentHelper.js
+++ lecture_3.5/aura/TouristAssignment/TouristAssignmentCommunityComponentHelper.js
@@ -54,14 +54,12 @@
     },
     
     getURLParameterValue: function() {
-        var querystring = location.search.substr(1);
-        var paramValue = {};
+        let querystring = location.search.substr(1);
+        let paramValue = {};
         querystring.split("&").forEach(function(part) {
-            var param = part.split("=");
+            let param = part.split("=");
             paramValue[param[0]] = decodeURIComponent(param[1]);
-        });
-        
-        console.log('paramValue-' + paramValue);
+        });      
         return paramValue;
     }
 })
