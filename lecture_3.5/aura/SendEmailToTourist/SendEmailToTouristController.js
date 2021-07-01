d8739d0dfa63760e50fc2efb810e78b797428ab1
VyacheslavRadyuk <vyacheslavradyuk@mail.ru>
2021-07-01 17:12:43 +0300

    remove to custom labels

--- lecture_3.5/aura/SendEmailToTourist/SendEmailToTouristController.js
+++ lecture_3.5/aura/SendEmailToTourist/SendEmailToTouristController.js
@@ -2,8 +2,8 @@
     sendEmail : function(component, event, helper) {
         let getRecordId = component.get('v.recordId');
         let getEmail = component.get('v.tourist').Email__c;
-        let getSubject = 'Hello, link for travel registration';
-        let getBody = 'https://eponec-developer-edition.ap24.force.com/Eponec/s/?id=' + getRecordId; 
+        let getSubject = $A.get("$Label.c.subjectEmailToTourist");
+        let getBody = $A.get("$Label.c.linkToTheCommunityPageEponec") + getRecordId; 
         helper.sendEmailHelper(component, event, getEmail, getSubject, getBody);   
     }
 })
